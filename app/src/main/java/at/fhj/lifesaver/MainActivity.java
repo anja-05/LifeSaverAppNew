682ab8e29b7a230a8f52543eef64b42f851a0535
anja-05 <a-schloffer@gmx.at>
2025-05-19 16:11:53 +0200

    feature (ui): styled BottomNavigationView with custom selector and red top indicator on active tab feature (ui): created login screen with email and password inputs

--- app/src/main/java/at/fhj/lifesaver/MainActivity.java
+++ app/src/main/java/at/fhj/lifesaver/MainActivity.java
@@ -7,18 +7,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import at.fhj.lifesaver.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new LernenFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.lektion:
+                    replaceFragment(new LernenFragment());
+                    break;
+                case R.id.uebung:
+                    replaceFragment(new UebungFragment());
+                    break;
+                case R.id.profil:
+                    replaceFragment(new ProfilFragment());
+                    break;
+            }
+            return true;
         });
+
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
